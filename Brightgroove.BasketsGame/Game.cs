605bb05c9e2097ac6fd61ed09e52fa461b2319fa
Michael Kharchenko <kharchenkoms@gmail.com>
2018-10-16 20:39:12 +0300

    added Game class

---
+++ Brightgroove.BasketsGame/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brightgroove.BasketsGame
+{
+    public class Game
+    {
+        private bool _isStopped = false;
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
